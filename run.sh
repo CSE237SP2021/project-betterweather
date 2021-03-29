025155ff291b8d36da3745cf633d103bf274a722
MandoQ <armando.quintana@wustl.edu>
2021-03-29 14:08:00 -0500

    Added test functionality to script
    Added junit jar and edited the script to allow junit tests to run in the command line

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#runs our project
 javac -cp lib/java-json.jar src/betterWeather/*.java
 java -cp "src:lib/*" betterWeather.WeatherApp
+
+echo "Now running tests..."
+
+#runs our tests 
+javac -cp "lib/*" src/betterWeather/*.java
+javac -cp "src:lib/*" src/tests/*.java
+
+echo "If no errors appear, then our tests successfully ran!"
